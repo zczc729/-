1df7d7af536b8254cb65a773eb33cdd604c6c18e
사과톡톡톡 <102545749+zczc729@users.noreply.github.com>
2024-05-29 14:14:44 +0900

    Add files via upload

--- python/test.py
+++ python/test.py
@@ -1,48 +1,18 @@
-"""
-ls = [[1, 0, 1], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5]]
+num = 2
 
-a = 0
+print("\t\t\t\t\t\t구구단\n")
+print("   2단\t\t   3단\t\t   4단\t\t   5단\t\t   6단\t\t   7단\t\t   8단\t\t   9단")
 
-for i in ls:
-    a += i.count(1)
-
-print(a)
-"""
-
-"""
-
+for i in range(1, 10):
+    while num <= 9:
+        print(f"|{num} * {i} = {num * i}|", end="\t")
+        num += 1
+    num = 2
+    print()
 
 
-num1 = int(input("시작 번호를 입력하시오. : "))
-num2 = int(input("끝 번호를 입력하시오 : "))
-
-ls = []
-for i in range(num1, num2+1, 1):
-    ls.append([int(a) for a in str(i)])
-print(ls)
-x = 0
-flag = True
-while flag:
-    sum = 0
-    for i in range(len(ls)):
-        for j in range(len(ls[i])):
-            if x == ls[i][j]:
-                sum += 1
-    print("{}:{}개".format(x, sum), end = ", ")
-    x += 1
-    if x == 10:
-        flag = False
-"""
 
 
-num1 = int(input("시작 번호를 입력하시오. : "))
-num2 = int(input("끝 번호를 입력하시오 : "))
 
-count = {x:0 for x in range(0,10)}
-for x in range(num1, num2):
-    for i in str(x):
-        count[int(i)]+=1
-for i in count:
-    # print("{}:{}개".format(i, count.get(i)), end = ", ")
-    print(f"{i}:{count.get(i)}개", end = ", ")
+
 
